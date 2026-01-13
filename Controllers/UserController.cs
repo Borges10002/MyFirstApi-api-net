84d36be8abdbb0cdd8d83f0189bab70f806c80dc
Borges10002 <borges10002@gmail.com>
2026-01-12 20:49:03 -0400

    I send my first project to git

---
+++ Controllers/UserController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApi3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
