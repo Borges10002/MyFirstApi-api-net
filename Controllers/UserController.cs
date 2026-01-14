2b00c8cfbcc97f1693bf7f2a478bc3a93c4003ac
Borges10002 <borges10002@gmail.com>
2026-01-14 19:44:38 -0400

    update

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-namespace MyFirstApi3.Controllers
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
-    {
+namespace MyFirstApi.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpGet]
+    [Route("{id}")]
+    [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status400BadRequest)]
+    public IActionResult GetById([FromRoute] int id)
+    {
+        var response = new User
+        {
+            Id = 1,
+            Age = 7,
+            Name = "welisson"
+        };
+
+        return Ok(response);
     }
 }
