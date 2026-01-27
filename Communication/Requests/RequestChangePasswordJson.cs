0f5fbeb5939cdbb13693c6bc90a9ac8b95584012
Borges10002 <borges10002@gmail.com>
2026-01-27 19:59:56 -0400

    routes

---
+++ Communication/Requests/RequestChangePasswordJson.cs
@@ -0,0 +1,8 @@
+﻿namespace MyFirstApi3.Communication.Requests
+{
+    public class RequestChangePasswordJson
+    {
+        public string CurrentPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
